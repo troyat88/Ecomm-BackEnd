7214c35ca5f3a4c93c62fab94051776102a2148d
Troy <troyat88@gmail.com>
2021-09-13 21:38:21 -0700

    completed models and config

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,39 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-  },
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate: {
+        isDecimal:true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric:true,
+      }
+    },
+    category_id: {  //Connecting to Category Model's ID
+     type: DataTypes.INTEGER, 
+     references: {
+       model: "category",
+       key:"id", 
+    }
+  }, 
+},
   {
     sequelize,
     timestamps: false,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_id: {  //Connecting to Product Model's ID
+      type: DataTypes.INTEGER, 
+      references: {
+        model: "product",
+        key:"id", 
+     }
+   },
+   category_id: {  //Connecting to Tag Model's ID
+    type: DataTypes.INTEGER, 
+    references: {
+      model: "tag",
+      key:"id", 
+   }
+ },  
   },
   {
     sequelize,
