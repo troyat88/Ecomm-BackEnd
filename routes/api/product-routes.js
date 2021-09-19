a13c5eb20a57e5588fbc3b9b9245878d078ade2b
Troy <troyat88@gmail.com>
2021-09-18 22:20:25 -0700

    added demo vids changed readme

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -55,10 +55,10 @@ router.post("/", (req, res) => {
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        const productTagIdArr = req.body.tagIds.map((category_id) => {
           return {
             product_id: product.id,
-            tag_id,
+            category_id,
           };
         });
         return ProductTag.bulkCreate(productTagIdArr);
